a2730899f5495dae4fadbb86d883877f9fe3bf68
ierddan <ierddan_c@yahoo.com.br>
2023-07-26 14:20:34 -0300

    Initial commit

---
+++ src/test/java/com/ierddan/architecture/hexagonal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ierddan.architecture.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
